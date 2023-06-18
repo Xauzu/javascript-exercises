80fabaad4b19bbc6a19db61739d4e561e5967d3c
Xau <frozzy.xfs@gmail.com>
2023-06-18 08:10:24 -0400

    Add removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removal) {
+    let output = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!removal.includes(array[i])) {
+            output.push(array[i]);
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
