de24a82ce6f9e02af024aee6b4a532901b0181f5
Xau <frozzy.xfs@gmail.com>
2023-06-18 08:29:52 -0400

    Add larger argument first check

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,16 @@ const sumAll = function(start, end) {
         return "ERROR";
     }
 
+    // Reversed argument check
+    if (start > end) {
+        let temp = start;
+        console.log(temp);
+        start = end;
+        console.log(start);
+        end = temp;
+        console.log(end);
+    }
+
     let sum = 0;
     for (let i = start; i <= end; i++) {
         sum += i;
