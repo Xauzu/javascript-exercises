3c7ec605e5e2d5f85e25d20e5fdb6d9e0f694e20
Xau <frozzy.xfs@gmail.com>
2023-06-26 20:27:44 -0400

    Enabled all tests and Add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = series => {
+    if (series < 0) return "OOPS";
+    if (series <= 2) return 1;
+    
+    let num1 = 1;
+    let num2 = 1;
 
-};
+    let currentSeries = 3;
+    while(currentSeries <= series) {
+        const newNum = num1 + num2;
+        num1 = num2;
+        num2 = newNum;
+        currentSeries++;
+    }
+    return num2;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
