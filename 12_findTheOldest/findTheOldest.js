c060b27147a9063783e9c41c2589d20e4dd86e98
Xau <frozzy.xfs@gmail.com>
2023-06-26 20:51:44 -0400

    Add findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = people => {
+    const ordered = people.sort((a, b) => {
+        const last = a.yearOfDeath - a.yearOfBirth;
+        const next = b.yearOfDeath - b.yearOfBirth;
+  
+        return last > next ? 1 : -1;
+    });
+    return ordered[ordered.length-1];
 };
 
 // Do not edit below this line
