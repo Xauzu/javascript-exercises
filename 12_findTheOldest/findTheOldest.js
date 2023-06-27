e3e68b5efaf39a930f0c3f6144ac87d5a8b0f59e
Xau <frozzy.xfs@gmail.com>
2023-06-26 21:11:11 -0400

    Fix ordering calculation error

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,13 @@
 const findTheOldest = people => {
     const ordered = people.sort((a, b) => {
-        const last = a.yearOfDeath - a.yearOfBirth;
-        const next = b.yearOfDeath - b.yearOfBirth;
+        let aDeath = a.yearOfDeath;
+        let bDeath = b.yearOfDeath;
+
+        if (!aDeath) aDeath = (new Date()).getFullYear();
+        if (!bDeath) bDeath = (new Date()).getFullYear();
+
+        const last = aDeath - a.yearOfBirth;
+        const next = bDeath - b.yearOfBirth;
   
         return last > next ? 1 : -1;
     });
