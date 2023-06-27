ea84923e63d89b9669dbe8885da8639500d56a22
Xau <frozzy.xfs@gmail.com>
2023-06-26 20:42:29 -0400

    Convert to use map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,4 @@
-const getTheTitles = books => {
-    let titles = []
-    books.forEach(book => {
-        titles.push(book.title);
-    });
-    return titles;
-};
+const getTheTitles = books => books.map((book) => book.title);
 
 // Do not edit below this line
 module.exports = getTheTitles;
