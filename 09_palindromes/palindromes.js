1a30205dfbe5ba5afb82a37cc8bbf730f95157ce
Xau <frozzy.xfs@gmail.com>
2023-06-26 20:10:11 -0400

    Add palindromes functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = (input) => {
+    const ignore = [' ', '.', ',', '!']
+    let start = 0;
+    let end = input.length - 1;
 
+    while (start < end) {
+        // Moves the start and end index if the character at the index is a character to be ignored
+        while (ignore.includes(input[start])) start++;
+        while (ignore.includes(input[end])) end--;
+        
+        if (input[start].toLowerCase() !== input[end].toLowerCase()) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
